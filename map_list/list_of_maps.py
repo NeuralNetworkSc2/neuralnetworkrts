8e05582e8de13abaad23ba2f25892ec07fb03b8e
Yaroslav Yastremskiy <yarik_02022005@mail.ru>
2023-04-25 23:22:19 +0300

    Added function for creating a list of maps

---
+++ map_list/list_of_maps.py
@@ -0,0 +1,21 @@
+import os, fnmatch
+
+
+def find(extension, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, extension):
+                temp = os.path.join(root, name).split('\\')
+                result.append(temp[-1])
+    return result
+
+print('Set the path to the map folder: ')
+path = input()
+all_maps = find('*.SC2Map', path)
+map_count = len([map for map in all_maps if map != 0])
+if map_count == 0:
+    print('In this directory no maps, try again')
+else:
+    print(map_count)
+print(all_maps)
